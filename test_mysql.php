8a9be3511bab128c7ebd77ef1344a6e774ae5575
juanmarrom <juan.martinez.02912@uols.org>
2018-06-01 13:23:38 +0200

    Create test_mysql.php

---
+++ test_mysql.php
@@ -0,0 +1,24 @@
+<?php
+	require_once './clases/util.php';			
+	Util::iniciarConexion("./conf.txt");
+	$conn =  Util::getConexion();
+	$sql = "Select ID, NOMBRE FROM EMPRESA WHERE NOMBRE LIKE ?";	
+	$sql_insert = "INSERT INTO AUDITORIA_BUSQUEDA (ID_USUARIO, TIEMPO, RESULTADO) VALUES (?,?,?);";
+		try {
+			$stmt = $conn->prepare("$sql");
+			$stmt->bind_param("s", "%Za%");
+			$stmt->execute();
+			$result = $stmt->get_result();
+			$número_filas = mysql_num_rows($result);
+			echo "número_filas - $número_filas\n";
+			while ($row = $result->fetch_assoc()){
+				$stmt2 = $conn->prepare("$sql_insert");
+				$stmt2->bind_param("iii", 1, 10,5);
+				$stmt2->execute();
+				$stmt2->close();
+			}						
+		} 
+		catch(Exception $e) {
+			echo "Exception - $e\n";
+		}					
+?>
