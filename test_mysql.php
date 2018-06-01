7533bcab9104622b566e751553989b5ce15b5fc4
juanmarrom <juan.martinez.02912@uols.org>
2018-06-01 07:35:27 -0700

    PRUEBAS

--- test_mysql.php
+++ test_mysql.php
@@ -2,18 +2,24 @@
 	require_once './clases/util.php';			
 	Util::iniciarConexion("./conf.txt");
 	$conn =  Util::getConexion();
-	$sql = "Select ID, NOMBRE FROM EMPRESA WHERE NOMBRE LIKE %?%";	
+	$sql = "Select ID, NOMBRE FROM EMPRESA WHERE NOMBRE like ? limit 10";	
 	$sql_insert = "INSERT INTO AUDITORIA_BUSQUEDA (ID_USUARIO, TIEMPO, RESULTADO) VALUES (?,?,?);";
 		try {
 			$stmt = $conn->prepare("$sql");
-			$stmt->bind_param("s", "Za");
+			$nom = "%zara%";
+			$stmt->bind_param("s", $nom);
 			$stmt->execute();
+			$stmt->bind_result($id, $nombre);
+			
 			$result = $stmt->get_result();
-			$numero_filas = mysql_num_rows($result);
-			echo "número_filas - $número_filas\n";
+			$numero_filas =  $result->num_rows;
+			echo "número_filas - $numero_filas\n";
 			while ($row = $result->fetch_assoc()){
 				$stmt2 = $conn->prepare("$sql_insert");
-				$stmt2->bind_param("iii", 1, 10,5);
+				$num1 = 1;
+				$num2 = 10;
+				$num3 = 5;
+				$stmt2->bind_param("iii", $num1, $num2, $num3);
 				$stmt2->execute();
 				$stmt2->close();
 			}						
